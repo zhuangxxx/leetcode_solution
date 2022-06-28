b1e8c2173875c212b44b6c6559a26fdf3dce5a7a
zhuangxxx <zhuangxxx@github.com>
2022-06-28 23:17:12 +0800

    Day 11, Symmetric Tree.

--- src/leet_code/mod.rs
+++ src/leet_code/mod.rs
@@ -1,4 +1,5 @@
 pub mod p100_same_tree;
+pub mod p101_symmetric_tree;
 pub mod p13_roman_to_integer;
 pub mod p14_longest_common_prefix;
 pub mod p1_two_sum;
---
+++ src/leet_code/p101_symmetric_tree.rs
@@ -0,0 +1,126 @@
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+struct Solution;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        if let Some(root) = root {
+            let mut queue = std::collections::VecDeque::new();
+            queue.push_back(root.borrow().left.clone());
+            queue.push_back(root.borrow().right.clone());
+
+            while !queue.is_empty() {
+                if let (Some(p), Some(q)) = (queue.pop_front(), queue.pop_front()) {
+                    if p.is_none() && q.is_none() {
+                        continue;
+                    }
+
+                    if p.is_none() || q.is_none() {
+                        return false;
+                    }
+
+                    if let (Some(p), Some(q)) = (p, q) {
+                        if p.borrow().val != q.borrow().val {
+                            return false;
+                        }
+
+                        queue.push_back(p.borrow().left.clone());
+                        queue.push_back(q.borrow().right.clone());
+
+                        queue.push_back(p.borrow().right.clone());
+                        queue.push_back(q.borrow().left.clone());
+                    }
+                }
+            }
+        }
+
+        true
+        // 0ms/2.1MB
+    }
+
+    // fn is_symmetric_node(
+    //     p: Option<Rc<RefCell<TreeNode>>>,
+    //     q: Option<Rc<RefCell<TreeNode>>>,
+    // ) -> bool {
+    //     match (p, q) {
+    //         (None, None) => true,
+    //         (Some(_), None) | (None, Some(_)) => false,
+    //         (Some(p), Some(q)) => {
+    //             p.borrow().val == q.borrow().val
+    //                 && Self::is_symmetric_node(p.borrow().left.clone(), q.borrow().right.clone())
+    //                 && Self::is_symmetric_node(p.borrow().right.clone(), q.borrow().left.clone())
+    //         }
+    //     }
+    // }
+
+    // pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+    //     if let Some(root) = root {
+    //         Self::is_symmetric_node(root.borrow().left.clone(), root.borrow().right.clone())
+    //     } else {
+    //         true
+    //     }
+    //     // 0ms/2.2MB
+    // }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert!(Solution::is_symmetric(Some(Rc::new(RefCell::new(
+            TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+                    right: Some(Rc::new(RefCell::new(TreeNode::new(4))))
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: Some(Rc::new(RefCell::new(TreeNode::new(4)))),
+                    right: Some(Rc::new(RefCell::new(TreeNode::new(3))))
+                })))
+            }
+        )))));
+    }
+
+    #[test]
+    fn test2() {
+        assert!(!Solution::is_symmetric(Some(Rc::new(RefCell::new(
+            TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode::new(3))))
+                }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 2,
+                    left: None,
+                    right: Some(Rc::new(RefCell::new(TreeNode::new(3))))
+                })))
+            }
+        )))));
+    }
+}
