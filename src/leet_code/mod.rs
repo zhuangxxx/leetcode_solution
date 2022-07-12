adb42b06081175008840be23181fab68d36008a2
zhuangxxx <zhuangxxx@github.com>
2022-07-12 23:22:30 +0800

    Day 23, Implement Queue using Stacks.

--- src/leet_code/mod.rs
+++ src/leet_code/mod.rs
@@ -32,6 +32,7 @@ pub mod p225_implement_stack_using_queues;
 pub mod p226_invert_binary_tree;
 pub mod p228_summary_ranges;
 pub mod p231_power_of_two;
+pub mod p232_implement_queue_using_stacks;
 pub mod p26_remove_duplicates_from_sorted_array;
 pub mod p27_remove_element;
 pub mod p28_implement_strstr;
---
+++ src/leet_code/p232_implement_queue_using_stacks.rs
@@ -0,0 +1,57 @@
+/// [232. 用栈实现队列](https://leetcode.cn/problems/implement-queue-using-stacks/)
+struct MyQueue {
+    ord: std::collections::VecDeque<i32>,
+    rev: std::collections::VecDeque<i32>,
+    // 0ms/2.2MB
+}
+
+impl MyQueue {
+    fn new() -> Self {
+        MyQueue {
+            ord: std::collections::VecDeque::new(),
+            rev: std::collections::VecDeque::new(),
+        }
+    }
+
+    fn push(&mut self, x: i32) {
+        self.ord.push_back(x);
+        self.rev = std::collections::VecDeque::new();
+
+        for i in (0..self.ord.len()).rev() {
+            self.rev.push_back(self.ord[i]);
+        }
+    }
+
+    fn pop(&mut self) -> i32 {
+        self.ord = std::collections::VecDeque::new();
+
+        for i in (0..self.rev.len() - 1).rev() {
+            self.ord.push_back(self.rev[i])
+        }
+
+        self.rev.pop_back().unwrap()
+    }
+
+    fn peek(&self) -> i32 {
+        self.ord[0]
+    }
+
+    fn empty(&self) -> bool {
+        self.ord.len() == 0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut my_queue = MyQueue::new();
+        my_queue.push(1);
+        my_queue.push(2);
+        assert_eq!(my_queue.peek(), 1);
+        assert_eq!(my_queue.pop(), 1);
+        assert!(!my_queue.empty());
+    }
+}
