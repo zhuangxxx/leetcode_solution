c67ae3b809d764003d425529c22b3d6e94ba9db2
zhuangxxx <zhuangxxx@github.com>
2022-07-15 22:58:32 +0800

    Day 26, Valid Anagram.

--- src/leet_code/p235_lowest_common_ancestor_of_a_binary_search_tree.rs
+++ src/leet_code/p235_lowest_common_ancestor_of_a_binary_search_tree.rs
@@ -5,6 +5,7 @@ struct Solution;
 use std::cell::RefCell;
 use std::rc::Rc;
 impl Solution {
+    /// [235. 二叉搜索树的最近公共祖先](https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/)
     pub fn lowest_common_ancestor(
         root: Option<Rc<RefCell<TreeNode>>>,
         p: Option<Rc<RefCell<TreeNode>>>,
---
+++ src/leet_code/p243_valid_anagram.rs
@@ -0,0 +1,53 @@
+struct Solution;
+
+impl Solution {
+    /// [242. 有效的字母异位词](https://leetcode.cn/problems/valid-anagram/)
+    pub fn is_anagram(s: String, t: String) -> bool {
+        if s.len() != t.len() {
+            return false;
+        }
+
+        let mut bytes = [0; 255];
+
+        let (s, t) = (s.as_bytes(), t.as_bytes());
+        for i in 0..s.len() {
+            bytes[s[i] as usize] += 1;
+            bytes[t[i] as usize] -= 1;
+        }
+
+        bytes.iter().all(|&x| x == 0)
+        // 0ms/2.2MB
+    }
+
+    // pub fn is_anagram(s: String, t: String) -> bool {
+    //     let (mut s, mut t) = (s.chars().collect::<Vec<_>>(), t.chars().collect::<Vec<_>>());
+    //     s.sort();
+    //     t.sort();
+
+    //     s == t
+    //     // 8ms/2.6MB
+    // }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert!(Solution::is_anagram(
+            "anagram".to_string(),
+            "nagaram".to_string()
+        ));
+    }
+
+    #[test]
+    fn test2() {
+        assert!(!Solution::is_anagram("rat".to_string(), "car".to_string()));
+    }
+
+    #[test]
+    fn fail1() {
+        assert!(!Solution::is_anagram("ac".to_string(), "bb".to_string()));
+    }
+}
