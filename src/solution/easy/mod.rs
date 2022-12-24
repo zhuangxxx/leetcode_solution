b1dfb9ee96b148b3af163651600732a1746c93b9
zhuangxxx <zhuangxxx@github.com>
2022-12-24 23:02:47 +0800

    Day 85, Sort Array By Parity II.

--- src/solution/easy/mod.rs
+++ src/solution/easy/mod.rs
@@ -169,6 +169,7 @@ pub mod p897_increasing_order_search_tree;
 pub mod p905_sort_array_by_parity;
 pub mod p908_smallest_range_i;
 pub mod p914_x_of_a_kind_in_a_deck_of_cards;
+pub mod p917_reverse_only_letters;
+pub mod p922_sort_array_by_parity_ii;
 pub mod p94_binary_tree_inorder_traversal;
 pub mod p9_palindrome_number;
-pub mod p917_reverse_only_letters;
---
+++ src/solution/easy/p922_sort_array_by_parity_ii.rs
@@ -0,0 +1,55 @@
+use core::num;
+
+struct Solution;
+
+impl Solution {
+    /// [922. 按奇偶排序数组 II](https://leetcode.cn/problems/sort-array-by-parity-ii/)
+    pub fn sort_array_by_parity_ii(nums: Vec<i32>) -> Vec<i32> {
+        nums.iter()
+            .filter(|&x| x & 1 == 0)
+            .zip(nums.iter().filter(|&x| x & 1 != 0))
+            .map(|(&e, &o)| vec![e, o])
+            .flatten()
+            .collect()
+        // 8ms/2.2MB
+    }
+
+    // pub fn sort_array_by_parity_ii(mut nums: Vec<i32>) -> Vec<i32> {
+    //     let (mut e, mut o) = (0, 1);
+    //     loop {
+    //         while e <= nums.len() - 2 && nums[e] & 1 == 0 {
+    //             e += 2;
+    //         }
+    //         while o <= nums.len() - 1 && nums[o] & 1 == 1 {
+    //             o += 2;
+    //         }
+    //         if e > nums.len() - 2 || o > nums.len() - 1 {
+    //             break;
+    //         }
+    //         nums.swap(e, o);
+    //         e += 2;
+    //         o += 2;
+    //     }
+
+    //     nums
+    //     // 8ms/2.1MB
+    // }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(
+            Solution::sort_array_by_parity_ii(vec![4, 2, 5, 7]),
+            vec![4, 5, 2, 7]
+        );
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(Solution::sort_array_by_parity_ii(vec![2, 3]), vec![2, 3]);
+    }
+}
