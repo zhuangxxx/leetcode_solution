f1261042d6f4d2fe2886c87e2dc81fb82c995068
zhuangxxx <zhuangxxx@github.com>
2023-04-18 21:53:37 +0800

    Day 124, Word Search(Optimize Space Complexity).

--- src/solution/medium/p79_word_search.rs
+++ src/solution/medium/p79_word_search.rs
@@ -2,10 +2,9 @@ struct Solution;
 
 impl Solution {
     /// [79. 单词搜索](https://leetcode.cn/problems/word-search/)
-    pub fn exist(board: Vec<Vec<char>>, word: String) -> bool {
+    pub fn exist(mut board: Vec<Vec<char>>, word: String) -> bool {
         fn check(
-            board: &Vec<Vec<char>>,
-            visit: &mut Vec<Vec<bool>>,
+            board: &mut Vec<Vec<char>>,
             index: (usize, usize),
             chars: &Vec<char>,
             len: usize,
@@ -16,7 +15,7 @@ impl Solution {
                 return true;
             }
 
-            visit[index.0][index.1] = true;
+            board[index.0][index.1] = ' ';
             for ops in OPS {
                 let (i, j) = match ops {
                     ('-', ' ') => {
@@ -45,30 +44,97 @@ impl Solution {
                     }
                     _ => continue,
                 };
-                if !visit[i][j] && board[i][j] == chars[len] {
-                    if check(board, visit, (i, j), chars, len + 1) {
+                if board[i][j] == chars[len] {
+                    if check(board, (i, j), chars, len + 1) {
                         return true;
                     }
                 }
             }
-            visit[index.0][index.1] = false;
+            board[index.0][index.1] = chars[len - 1];
 
             false
         }
 
-        let mut visit = vec![vec![false; board[0].len()]; board.len()];
         let chars = word.chars().collect::<Vec<_>>();
         for i in 0..board.len() {
             for j in 0..board[0].len() {
-                if board[i][j] == chars[0] && check(&board, &mut visit, (i, j), &chars, 1) {
+                if board[i][j] == chars[0] && check(&mut board, (i, j), &chars, 1) {
                     return true;
                 }
             }
         }
 
         false
-        // 128ms/2.2MB
+        // 96ms/2.1MB
     }
+
+    // pub fn exist(board: Vec<Vec<char>>, word: String) -> bool {
+    //     fn check(
+    //         board: &Vec<Vec<char>>,
+    //         visit: &mut Vec<Vec<bool>>,
+    //         index: (usize, usize),
+    //         chars: &Vec<char>,
+    //         len: usize,
+    //     ) -> bool {
+    //         const OPS: [(char, char); 4] = [('-', ' '), ('+', ' '), (' ', '-'), (' ', '+')];
+
+    //         if len == chars.len() {
+    //             return true;
+    //         }
+
+    //         visit[index.0][index.1] = true;
+    //         for ops in OPS {
+    //             let (i, j) = match ops {
+    //                 ('-', ' ') => {
+    //                     if index.0 == 0 {
+    //                         continue;
+    //                     }
+    //                     (index.0 - 1, index.1)
+    //                 }
+    //                 ('+', ' ') => {
+    //                     if index.0 == board.len() - 1 {
+    //                         continue;
+    //                     }
+    //                     (index.0 + 1, index.1)
+    //                 }
+    //                 (' ', '-') => {
+    //                     if index.1 == 0 {
+    //                         continue;
+    //                     }
+    //                     (index.0, index.1 - 1)
+    //                 }
+    //                 (' ', '+') => {
+    //                     if index.1 == board[0].len() - 1 {
+    //                         continue;
+    //                     }
+    //                     (index.0, index.1 + 1)
+    //                 }
+    //                 _ => continue,
+    //             };
+    //             if !visit[i][j] && board[i][j] == chars[len] {
+    //                 if check(board, visit, (i, j), chars, len + 1) {
+    //                     return true;
+    //                 }
+    //             }
+    //         }
+    //         visit[index.0][index.1] = false;
+
+    //         false
+    //     }
+
+    //     let mut visit = vec![vec![false; board[0].len()]; board.len()];
+    //     let chars = word.chars().collect::<Vec<_>>();
+    //     for i in 0..board.len() {
+    //         for j in 0..board[0].len() {
+    //             if board[i][j] == chars[0] && check(&board, &mut visit, (i, j), &chars, 1) {
+    //                 return true;
+    //             }
+    //         }
+    //     }
+
+    //     false
+    //     // 128ms/2.2MB
+    // }
 }
 
 #[cfg(test)]
