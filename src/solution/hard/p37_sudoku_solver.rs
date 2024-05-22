722e77be7cc4fb81d88100b8e57c69406131b2b2
zhuangxxx <zhuangxxx@github.com>
2024-05-22 23:02:21 +0800

    Day 175, Sudoku Solver(Bitwise Operation Optimization).

--- src/solution/hard/p37_sudoku_solver.rs
+++ src/solution/hard/p37_sudoku_solver.rs
@@ -5,63 +5,117 @@ impl Solution {
     pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
         fn bt(
             board: &mut Vec<Vec<char>>,
-            col: &mut [[bool; 10]],
-            row: &mut [[bool; 10]],
-            grid: &mut [[bool; 10]],
-            mut i: usize,
-            mut j: usize,
+            pos: &mut Vec<(usize, usize)>,
+            row: &mut Vec<u16>,
+            col: &mut Vec<u16>,
+            grid: &mut Vec<u16>,
         ) -> bool {
-            if j == 9 {
-                i += 1;
-                j = 0;
-            }
-            if i == 9 {
-                return true;
-            }
-
-            let k = i / 3 * 3 + j / 3 % 3;
-            if board[i][j] != '.' {
-                let n = (board[i][j] as u8 - b'0') as usize;
-                col[j][n] = true;
-                row[i][n] = true;
-                grid[k][n] = true;
-                return bt(board, col, row, grid, i, j + 1);
-            }
-            for n in 1..=9 {
-                if row[i][n] || col[j][n] || grid[k][n] {
-                    continue;
+            if let Some((i, j)) = pos.pop() {
+                let k = i / 3 * 3 + j / 3;
+                let bit = !(row[i] | col[j] | grid[k]);
+                for digit in 0..9 {
+                    let mask = 1 << digit;
+                    if bit & mask != 0 {
+                        row[i] |= mask;
+                        col[j] |= mask;
+                        grid[k] |= mask;
+                        board[i][j] = (digit + b'1') as char;
+                        if bt(board, pos, row, col, grid) {
+                            return true;
+                        }
+                        board[i][j] = '.';
+                        grid[k] ^= mask;
+                        col[j] ^= mask;
+                        row[i] ^= mask;
+                    }
                 }
-                row[i][n] = true;
-                col[j][n] = true;
-                grid[k][n] = true;
-                board[i][j] = (n as u8 + b'0') as char;
-                if bt(board, col, row, grid, i, j + 1) {
-                    return true;
-                }
-                board[i][j] = '.';
-                grid[k][n] = false;
-                col[j][n] = false;
-                row[i][n] = false;
-            }
+                pos.push((i, j));
 
-            false
+                false
+            } else {
+                true
+            }
         }
 
-        let (mut row, mut col, mut grid) = ([[false; 10]; 9], [[false; 10]; 9], [[false; 10]; 9]);
+        let mut pos = Vec::new();
+        let (mut row, mut col, mut grid) = (vec![0; 9], vec![0; 9], vec![0; 9]);
         for i in 0..9 {
             for j in 0..9 {
-                if board[i][j] != '.' {
-                    let n = (board[i][j] as u8 - b'0') as usize;
-                    row[i][n] = true;
-                    col[j][n] = true;
-                    grid[i / 3 * 3 + j / 3 % 3][n] = true;
+                if board[i][j] == '.' {
+                    pos.push((i, j));
+                } else {
+                    let mask = 1 << (board[i][j] as u8 - b'1');
+                    let k = i / 3 * 3 + j / 3;
+                    row[i] |= mask;
+                    col[j] |= mask;
+                    grid[k] |= mask;
                 }
             }
         }
 
-        bt(board, &mut col, &mut row, &mut grid, 0, 0);
-        // 0ms/2.12MB
+        bt(board, &mut pos, &mut row, &mut col, &mut grid);
     }
+
+    // pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
+    //     fn bt(
+    //         board: &mut Vec<Vec<char>>,
+    //         row: &mut [[bool; 10]],
+    //         col: &mut [[bool; 10]],
+    //         grid: &mut [[bool; 10]],
+    //         mut i: usize,
+    //         mut j: usize,
+    //     ) -> bool {
+    //         if j == 9 {
+    //             i += 1;
+    //             j = 0;
+    //         }
+    //         if i == 9 {
+    //             return true;
+    //         }
+
+    //         let k = i / 3 * 3 + j / 3 % 3;
+    //         if board[i][j] != '.' {
+    //             let n = (board[i][j] as u8 - b'0') as usize;
+    //             row[i][n] = true;
+    //             col[j][n] = true;
+    //             grid[k][n] = true;
+    //             return bt(board, row, col, grid, i, j + 1);
+    //         }
+    //         for n in 1..=9 {
+    //             if row[i][n] || col[j][n] || grid[k][n] {
+    //                 continue;
+    //             }
+    //             row[i][n] = true;
+    //             col[j][n] = true;
+    //             grid[k][n] = true;
+    //             board[i][j] = (n as u8 + b'0') as char;
+    //             if bt(board, row, col, grid, i, j + 1) {
+    //                 return true;
+    //             }
+    //             board[i][j] = '.';
+    //             grid[k][n] = false;
+    //             col[j][n] = false;
+    //             row[i][n] = false;
+    //         }
+
+    //         false
+    //     }
+
+    //     let (mut row, mut col, mut grid) = ([[false; 10]; 9], [[false; 10]; 9], [[false; 10]; 9]);
+    //     for i in 0..9 {
+    //         for j in 0..9 {
+    //             if board[i][j] != '.' {
+    //                 let n = (board[i][j] as u8 - b'0') as usize;
+    //                 row[i][n] = true;
+    //                 col[j][n] = true;
+    //                 grid[i / 3 * 3 + j / 3 % 3][n] = true;
+    //             }
+    //         }
+    //     }
+
+    //     bt(board, &mut row, &mut col, &mut grid, 0, 0);
+    //     // 0ms/2.12MB
+    // }
 }
 
 #[cfg(test)]
